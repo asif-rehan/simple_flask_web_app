5421140518d09bda9f53a9d6a01f38b0074b3c7b
Asif Rehan <asif.rehan9@gmail.com>
2017-06-21 12:49:09 -0700

    work in progress: added POST, GET and mimetype usage examples

---
+++ flask_test.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask import request
+from flask import json
+from flask import Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "It Works!"
+
+
+@app.route('/hello', methods=['POST'])
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
+        
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
+
+
+@app.route('/new_hello', methods = ['GET'])
+def api_new_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    resp.headers['TestingURL'] = 'https://asifrehan.com'
+
+    return resp
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.route('/users/<userid>', methods = ['GET'])
+def api_users(userid):
+    users = {'1':'john', '2':'steve', '3':'bill'}
+    
+    if userid in users:
+        return jsonify({userid:users[userid]})
+    else:
+        return not_found()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
